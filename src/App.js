37931681700a4f1e12831f8c245c319b8200a0fa
asamoile <a.samoile@gmail.com>
2022-07-05 11:49:12 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import CostItem from "./components/CostItem";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const costs = [
+        {
+            date: new Date(2021, 7, 23),
+            description: "Комп",
+            amount: 999.99,
+        },
+        {
+            date: new Date(2021, 4, 12),
+            description: "MacBook",
+            amount: 2200,
+        },
+
+        {
+            date: new Date(2021, 2, 28),
+            description: "Джинсы",
+            amount: 49.99,
+        },
+    ];
+
+    return (
+        <div>
+            <h1>React Developer</h1>
+            <CostItem
+                date={costs[0].date}
+                description={costs[0].description}
+                amount={costs[0].amount}
+            />
+            <CostItem
+                date={costs[1].date}
+                description={costs[1].description}
+                amount={costs[1].amount}
+            />
+            <CostItem
+                date={costs[2].date}
+                description={costs[2].description}
+                amount={costs[2].amount}
+            />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/CostDate.js
@@ -0,0 +1,16 @@
+function CostDate(props) {
+    const month = props.date.toLocaleString("ru-RU", { month: "long" });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("ru-RU", { day: "2-digit" });
+
+    return (
+        <div className="cost-item__date">
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+
+        </div>
+    );
+}
+
+export default CostDate;
