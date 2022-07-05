ebe6b317e14057440d03376d55c69fb8b5fa865d
asamoile <a.samoile@gmail.com>
2022-07-05 14:24:55 +0600

    доделал интерфейс с ценами

--- src/components/CostDate.js
+++ src/components/CostDate.js
@@ -1,13 +1,16 @@
+import './CostDate.css'
+
 function CostDate(props) {
-    const month = props.date.toLocaleString("ru-RU", { month: "long" });
+    const month = props.date.toLocaleString("ru-RU", {month: "long"});
     const year = props.date.getFullYear();
-    const day = props.date.toLocaleString("ru-RU", { day: "2-digit" });
+    const day = props.date.toLocaleString("ru-RU", {day: "2-digit"});
+
 
     return (
-        <div className="cost-item__date">
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
+        <div className="cost-date">
+            <div className="cost-date__month">{month}</div>
+            <div className="cost-date__year">{year}</div>
+            <div className="cost-date__day">{day}</div>
         </div>
     );
 }
